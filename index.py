cf194ed167852119f15483623cd47e9946815ba6
weno42 <obroberts@gmail.com>
2016-06-09 18:27:59 -0400

    initial commit

---
+++ index.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+import random
+import nltk
+from markov import MarkovGenerator
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+
+@app.route('/text')
+def text():
+	text = request.args['text']
+	prefix_file = open("input/pref.txt")
+	prefixes = prefix_file.readlines()
+	file = open("input/"+text+".txt")
+	lines = file.readlines()
+	nountypes = ["NN", "NNP", "NNS", "NNPS"]
+	punc = [".",",",";","?","-",]
+	newlines = ""
+	linelist = []
+	for line in lines:
+		tokens = nltk.word_tokenize(line)
+		tagged = nltk.pos_tag(tokens)
+		newline = line
+		for idx, tag in enumerate(tagged):
+			print(idx, tag)
+			if any(tag[1] in n for n in nountypes):
+				newword = random.choice(prefixes).rstrip().lower() + tag[0]
+				newline = newline.replace(tag[0], newword)
+
+		newlines += newline
+		linelist.append( newline )
+		#newlines += "\n"
+
+	generator = MarkovGenerator(n=2, max=200)
+	generator.feed(newlines)
+	genpoem = generator.generate()
+	genpoem = genpoem.split('\n')
+	
+	return render_template(
+		"text.html", 
+		newtext=linelist[:10],
+		mark=genpoem
+	)
+
+if __name__ == '__main__':
+	app.run(debug=True)
