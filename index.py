ddb4c6fc740f64907fdf6b507f260135a4203c29
weno42 <obroberts@gmail.com>
2016-06-29 11:45:33 -0400

    updates text and inputs

--- index.py
+++ index.py
@@ -1,10 +1,5 @@
 from flask import Flask, request, render_template
-import random
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-from nltk.tokenize import word_tokenize
-from markov import MarkovGenerator
+
 
 app = Flask(__name__)
 
@@ -15,39 +10,15 @@ def index():
 
 @app.route('/text')
 def text():
-	text = request.args['text']
-	prefix_file = open("input/pref.txt")
-	prefixes = prefix_file.readlines()
-	file = open("input/"+text+".txt")
-	lines = file.readlines()
-	nountypes = ["NN", "NNP", "NNS", "NNPS"]
-	punc = [".",",",";","?","-",]
-	newlines = ""
-	linelist = []
-	for line in lines:
-		tokens = nltk.word_tokenize(line)
-		tagged = nltk.pos_tag(tokens)
-		newline = line
-		for idx, tag in enumerate(tagged):
-			print(idx, tag)
-			if any(tag[1] in n for n in nountypes):
-				newword = random.choice(prefixes).rstrip().lower() + tag[0]
-				newline = newline.replace(tag[0], newword)
-
-		newlines += newline
-		linelist.append( newline )
-		#newlines += "\n"
-
-	generator = MarkovGenerator(n=2, max=200)
-	generator.feed(newlines)
-	genpoem = generator.generate()
-	genpoem = genpoem.split('\n')
-	
+	import text
+	data = text.generateText( request.args['text'] )
+	print(data['poem'])
 	return render_template(
 		"text.html", 
-		newtext=linelist[:10],
-		mark=genpoem
+		newtext = data['lines'][:8],
+		mark = data['poem']
 	)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	#app.run(debug=True)
+	app.run()
---
+++ text.py
@@ -0,0 +1,63 @@
+import random
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import word_tokenize
+from markov import MarkovGenerator
+
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+def generateText(text):
+	prefix_file = open("input/pref.txt")
+	prefixes = prefix_file.readlines()
+	file = open("input/"+text+".txt")
+	lines = file.readlines()
+	nountypes = ["NN", "NNS"]
+	punc = [".",",",";","?","-",]
+	newtext = []
+	for line in lines:
+		#print "--", i
+		sents = nltk.sent_tokenize( line )
+		newgraf = ""
+		for sent in sents:
+			tokens = nltk.word_tokenize(sent)
+			tagged = nltk.pos_tag(tokens)
+			newsent = sent
+			for idx, tag in enumerate(tagged):
+				#print idx, tag
+				if any(tag[1] in n for n in nountypes):
+					newword = random.choice(prefixes).rstrip().lower() + tag[0]
+					newsent = newsent.replace(tag[0], newword)
+			newgraf += newsent + " "
+		newtext.append( newgraf )
+	
+	generator = MarkovGenerator(n=2, max=200)
+	for lin in newtext:
+		generator.feed( lin )
+	genpoem = generator.generate()
+	while len( genpoem ) < 100:
+		genpoem = generator.generate()
+	
+	print len( genpoem )
+	print genpoem
+	
+	poemsents = nltk.sent_tokenize( genpoem )
+	print len( poemsents )
+	print poemsents
+	print poemsents[:-1]
+
+	if len(poemsents) == 1:
+		return { 'lines': newtext, 'poem': poemsents }
+	if len(poemsents) > 0:
+		return { 'lines': newtext, 'poem': poemsents[:-1] }
+	else:
+		return { 'lines': newtext, 'poem': genpoem }
+
+if __name__ == '__main__':
+
+	data = generateText(sys.argv[1])
+	#for line in data['lines'][:8]:
+	#	print line
+	print data['poem']
