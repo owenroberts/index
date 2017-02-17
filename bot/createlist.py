40baf0e00b42cddaae3c909f9859d2fc5a36ecca
owenroberts <obr208@nyu.edu>
2017-02-17 18:23:24 -0500

    updates about page and uncomments the timer on word gallery

---
+++ bot/createlist.py
@@ -0,0 +1,18 @@
+import random
+prefix_file = open("../input/pref.txt")
+prefixes = prefix_file.readlines()
+nouns_file = open("../input/allnouns.txt")
+nouns = nouns_file.readlines()
+
+url = "http://www.indexindexindex.com/new/" 
+
+f = open('list.txt', 'w')
+
+for noun in nouns:
+	p = random.choice(prefixes).strip()
+	href = url + noun.strip() + "/" + p
+	line = p + noun.strip() + " " + href
+	print line
+	f.write(line)
+	f.write('\n')
+f.close()
