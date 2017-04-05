36790829531b1c7e2269f266ef77f10e0b67b056
owenroberts <obr208@nyu.edu>
2017-04-05 15:41:04 -0400

    inital commit to urltext

--- index.py
+++ index.py
@@ -109,13 +109,60 @@ def gen():
 @app.route('/text/<title>')
 def text(title):
 	import text
-	data = text.generateText( title )
+	text_from_file = text.load_text_from_file(title)
+	data = text.generate_text( text_from_file )
 	return render_template(
 		"text.html",
 		title = title,
 		newtext = data['lines'][:10],
 		mark = data['poem']
 	)
+
+@app.route('/fromurl')
+def from_url():
+	import urllib
+	import text
+	from bs4 import BeautifulSoup
+	print request.args['url']
+
+	f = urllib.urlopen( request.args['url'] ).read()
+	print f
+
+	try:
+		soup = BeautifulSoup( f, 'html.parser')
+		text_from_url = ""
+		for p in soup.find_all('p'):
+			print p
+			text_from_url += p.get_text()
+		data = text.generate_text( text_from_url )
+		return render_template(
+			"text.html",
+			title = soup.title.string,
+			newtext = data['lines'][:10],
+			mark = data['poem']
+		)
+	except:
+		return render_template(
+			"gen.html",
+			error = "Sorry, that URL did not load correctly.  Try another URL."
+		)
+	
+
+
+
+
+@app.route('/paste')
+def paste():
+	import text
+	data = text.generate_text( request.args['text'] )
+	return render_template(
+		"text.html",
+		title = "?",
+		newtext = data['lines'][:10],
+		mark = data['poem']
+	)
+
+
 
 @app.route('/gallery/word')
 def gallery_word():
